734ae7d222b6e4bd3a6bd7d9c169dfd863ead624
Jonah Ullman <jonahmullman@gmail.com>
2020-05-14 11:14:09 -0500

    moved store

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: 'AIzaSyCvtbUD5FkWmpDlk_eq5bXps3WFPrigQlk',
+  authDomain: 'join-743f8.firebaseapp.com',
+  databaseURL: 'https://join-743f8.firebaseio.com',
+  projectId: 'join-743f8',
+  storageBucket: 'join-743f8.appspot.com',
+  messagingSenderId: '469054078141',
+  appId: '1:469054078141:web:5ceb2f63e4edf699540bd4',
+  measurementId: 'G-PY8NVB4M88',
+};
