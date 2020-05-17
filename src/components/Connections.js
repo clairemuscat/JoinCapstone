b562c14494aec3f7694fd74c73c39bbf0e1802dc
Claire Muscat <clairemuscat94@gmail.com>
2020-05-17 16:12:55 -0500

    merged with matching; this now has startvideochat and connections updates

--- src/components/Connections.js
+++ src/components/Connections.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React from "react";
+import { useSelector } from "react-redux";
+import { generateCompoundUid } from "../utils";
 
 function Connections(props) {
-  return <div>A list of users you've connected with</div>;
+  const profile = useSelector((state) => state.profile);
+  const user = useSelector((state) => state.user);
+
+  return (
+    <div>
+      {profile.matches ? (
+        profile.matches.map((match) => {
+          let compound = generateCompoundUid(user.uid, match.id);
+          return (
+            <div key={match.id}>
+              <h1>{match.firstName}</h1>
+            </div>
+          );
+        })
+      ) : (
+        <h1>Loading...</h1>
+      )}
+    </div>
+  );
 }
 
 export default Connections;
