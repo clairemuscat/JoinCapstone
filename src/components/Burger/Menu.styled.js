5e57d8f339e52695fa81a4605b33f8cd55e764c1
Jonah Ullman <jonahmullman@gmail.com>
2020-05-23 13:34:20 -0500

    made basic navbar

--- src/components/Burger/Menu.styled.js
+++ src/components/Burger/Menu.styled.js
@@ -23,7 +23,7 @@ export const StyledMenu = styled.nav`
     font-size: 1rem;
     text-transform: uppercase;
     padding: 2rem 0;
-    font-weight: bold;
+    font-weight: bold;git ch
     letter-spacing: 0.5rem;
     color: white;
     text-decoration: none;
