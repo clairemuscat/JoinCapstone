9dd36544dbdac234830cf3263a82679b2e3d5709
Jonah Ullman <jonahmullman@gmail.com>
2020-05-25 16:13:54 -0500

    small tweaks

--- src/components/Calendar/Sidebar.js
+++ src/components/Calendar/Sidebar.js
@@ -1,60 +1,135 @@
-import React from 'react'
-import './sidebar.css'
-import {fetchMeetings} from '../../store/meetings'
-import {fetchUpcomingMeetings} from '../../store/upcomingMeetings'
-import {connect} from 'react-redux'
-import DeleteMeeting from './DeleteMeeting'
+import React from 'react';
+import './sidebar.css';
+import { fetchMeetings } from '../../store/meetings';
+import { fetchUpcomingMeetings } from '../../store/upcomingMeetings';
+import { connect } from 'react-redux';
+import DeleteMeeting from './DeleteMeeting';
 
-class Sidebar extends React.Component{
-    constructor(){
-        super()
-    }
+class Sidebar extends React.Component {
+  constructor() {
+    super();
+  }
 
+  componentDidMount() {
+    this.props.pendingMeetings(this.props.user);
+    this.props.upcomingMeetings(this.props.user);
+  }
 
-    componentDidMount(){
-        this.props.pendingMeetings(this.props.user)
-        this.props.upcomingMeetings(this.props.user)
-    }
+  render() {
+    const upcoming = this.props.upcoming;
+    const meetings = this.props.meetings;
+    const user = this.props.user;
+    upcoming.filter(
+      (meeting) => (meeting.start = new Date(meeting.start).toUTCString())
+    );
+    meetings.filter(
+      (meeting) => (meeting.start = new Date(meeting.start).toUTCString())
+    );
 
-
-    render(){
-        const upcoming = this.props.upcoming
-        const meetings = this.props.meetings
-        const user=this.props.user
-        upcoming.filter(meeting=> meeting.start = new Date(meeting.start).toUTCString())
-        meetings.filter(meeting=> meeting.start = new Date(meeting.start).toUTCString())
-        
-        return( <section className="sidebar">
+    return (
+      <section className="sidebar">
         <div className="sidebar-header">
-          <h1>
-            Meetings
-          </h1>
+          <h1>Meetings</h1>
         </div>
-        <div className='meeting-section'>
-        <div className='meeting-list'>
-        <h3>UPCOMING</h3>
-    {upcoming===[] ?'No Updoming Meetings': upcoming.map(meeting => meeting.inviteFirst ? <div className='upcoming-meeting'><div className='meeting-content'>Title: {meeting.title}<br/> Time: {meeting.start}<br/>attendee(s): {meeting.host},{meeting.inviteFirst} {meeting.inviteLast} </div><br/> <div className='meeting-buttons'> <button type='button' id='start-call-button'>Start Meeting</button><button type='button'>Reschedule</button><button type='button' className='delete-button' >Cancel</button></div></div>:<div>{meeting.title} @ {meeting.start}<br/>attendee(s): {meeting.host}</div>)}
+        <div className="meeting-section">
+          <div className="meeting-list">
+            <h3>UPCOMING</h3>
+            {upcoming === []
+              ? 'No Updoming Meetings'
+              : upcoming.map((meeting) =>
+                  meeting.inviteFirst ? (
+                    <div className="upcoming-meeting">
+                      <div className="meeting-content">
+                        Title: {meeting.title}
+                        <br /> Time: {meeting.start}
+                        <br />
+                        Attendee(s): {meeting.host}, {meeting.inviteFirst}{' '}
+                        {meeting.inviteLast}{' '}
+                      </div>
+                      <br />{' '}
+                      <div className="meeting-buttons">
+                        {' '}
+                        <button
+                          type="button"
+                          className="button meeting-button"
+                          id="start-call-button"
+                        >
+                          Start Meeting
+                        </button>
+                        <button
+                          type="button"
+                          className="button meeting-button"
+                          id="reschedule"
+                        >
+                          Reschedule
+                        </button>
+                        <button
+                          type="button"
+                          className="button delete-button meeting-button"
+                        >
+                          Cancel
+                        </button>
+                      </div>
+                    </div>
+                  ) : (
+                    <div>
+                      {meeting.title} @ {meeting.start}
+                      <br />
+                      attendee(s): {meeting.host}
+                    </div>
+                  )
+                )}
+          </div>
+          <div>
+            <h3 className="meeting-list">PENDING</h3>
+            {meetings.map((meeting) => {
+              if (meeting.hostId === user.uid) {
+                return (
+                  <div className="meeting">
+                    <div className="meeting-content">
+                      Title: {meeting.title}
+                      <br /> Time: {meeting.start} <br /> Invitation:
+                      {meeting.inviteFirst} {meeting.inviteLast}
+                    </div>
+                    <br />
+                    <DeleteMeeting
+                      meeting={meeting}
+                      host={meeting.host}
+                      user={user}
+                    />{' '}
+                  </div>
+                );
+              } else {
+                return (
+                  <div className="meeting">
+                    {' '}
+                    <div className="meeting-content">
+                      Title: {meeting.title}
+                      <br /> Time: {meeting.start} <br />
+                      invite request: {meeting.inviteFirst} {meeting.inviteLast}
+                    </div>
+                    <br />
+                    <DeleteMeeting meeting={meeting} user={user} />
+                  </div>
+                );
+              }
+            })}
+          </div>
         </div>
-        <div>
-        <h3 className='meeting-list'>PENDING</h3>
-        { meetings.map(meeting=>{if(meeting.hostId===user.uid){ return <div className='meeting'>< div className='meeting-content'>Title: {meeting.title}<br/> Time: {meeting.start} <br/> Invitation:{meeting.inviteFirst} {meeting.inviteLast}</div><br/><DeleteMeeting meeting={meeting} host={meeting.host} user={user}/> </div>} 
-        else{return <div className='meeting'> <div className='meeting-content'>Title: {meeting.title}<br/> Time: {meeting.start} <br/>invite request: {meeting.inviteFirst} {meeting.inviteLast}</div><br/><DeleteMeeting meeting={meeting} user={user}/></div>}})}
-        </div>
-        </div>
-      </section>)
-    }
+      </section>
+    );
+  }
 }
 
-const mapState = (state) =>({
-    meetings:state.meetings,
-    user:state.user,
-    upcoming:state.upcomingMeetings
-})
+const mapState = (state) => ({
+  meetings: state.meetings,
+  user: state.user,
+  upcoming: state.upcomingMeetings,
+});
 
-const mapDispatch = (dispatch)=>({
-    pendingMeetings:(user) => dispatch(fetchMeetings(user)),
-    upcomingMeetings:(user)=>dispatch(fetchUpcomingMeetings(user))
-})
+const mapDispatch = (dispatch) => ({
+  pendingMeetings: (user) => dispatch(fetchMeetings(user)),
+  upcomingMeetings: (user) => dispatch(fetchUpcomingMeetings(user)),
+});
 
-
-export default connect(mapState,mapDispatch)(Sidebar)
+export default connect(mapState, mapDispatch)(Sidebar);
--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -1,118 +1,124 @@
-import React from 'react'
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import timeGridPlugin from '@fullcalendar/timegrid'
-import interactionPlugin from '@fullcalendar/interaction'
-import MyInput from './MyInput'
-import Sidebar from './Sidebar'
-import UpdateEvent from './UpdateEvent'
-import swal from '@sweetalert/with-react'
-import {connect} from 'react-redux'
-import './sidebar.css'
-import  {fetchEvents,newEvent,changeEvent,deleteEvent} from '../../store/events'
+import React from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import MyInput from './MyInput';
+import Sidebar from './Sidebar';
+import UpdateEvent from './UpdateEvent';
+import swal from '@sweetalert/with-react';
+import { connect } from 'react-redux';
+import './sidebar.css';
+import {
+  fetchEvents,
+  newEvent,
+  changeEvent,
+  deleteEvent,
+} from '../../store/events';
 
+import './main.scss'; // webpack must be configured to do this
 
-import "./main.scss"; // webpack must be configured to do this
-
-
-
-class Calendar extends React.Component{
-  constructor(props){
-    super(props)
- 
+class Calendar extends React.Component {
+  constructor(props) {
+    super(props);
   }
 
- componentDidMount(){
-  this.props.getCalendar(this.props.user)
- }
+  componentDidMount() {
+    this.props.getCalendar(this.props.user);
+  }
 
- render(){
-   return(
-     <div className='calendar'>
-    <Sidebar/>
-    <div className='fullcalendar'>
-    <FullCalendar
-      defaultView="timeGridWeek"
-      header={{
-        left: "prev,next today",
-        center: "title",
-        right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek"
-      }}
-      plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-      editable={true}
-      weekends={true}
-      events={this.props.events}
-      eventClick={(info)=>{
-        swal({
-        title:info.event.title,
-        content: <UpdateEvent event={info.event}update={this.props.updateEvent}/>,
-        buttons:{
-          cancel:true,
-         confirm:{
-           text:'delete',
-           value:info,}
-        }
-      }).then(info =>{
-        swal({  title: "Are you sure?",
-        text: "Once deleted, you will not be able to recover this event!",
-        icon: "warning",
-        buttons: {
-          cancel:true,
-          confirm:{
-            text:'delete',
-            value:info
-          }
-        },
-        }). 
-        then((value)=>{
-          if(value){
-            this.props.removeEvent(value.event)
-          swal({
-            title:'Event Deleted',
-            icon:'success'
-          })
-          }
-        })
-      })}}
-
-      dateClick={(evt)=>  swal({
-        title:"Create Event",
-        content:<MyInput date={evt} />,
-        buttons:{
-          cancel:true,
-          confirm:'Add Event'
-        }
-      })
-   .then(val=>{
-     console.log('oranges', val.value)
-    this.props.addEvent(this.props.user,val.value)
-    swal({
-      title:'Event Created',
-      text:'Event: ' + val.value.title + ', was created!',
-      icon:'success'
-    })
-  })}
-    />
-    </div>
-   </div>
-  
-  )}
-
+  render() {
+    return (
+      <div className="calendar home-title">
+        <Sidebar />
+        <div className="fullcalendar">
+          <FullCalendar
+            defaultView="timeGridWeek"
+            header={{
+              left: 'prev,next today',
+              center: 'title',
+              right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek',
+            }}
+            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+            editable={true}
+            weekends={true}
+            events={this.props.events}
+            eventClick={(info) => {
+              swal({
+                title: info.event.title,
+                content: (
+                  <UpdateEvent
+                    event={info.event}
+                    update={this.props.updateEvent}
+                  />
+                ),
+                buttons: {
+                  cancel: true,
+                  confirm: {
+                    text: 'delete',
+                    value: info,
+                  },
+                },
+              }).then((info) => {
+                swal({
+                  title: 'Are you sure?',
+                  text:
+                    'Once deleted, you will not be able to recover this event!',
+                  icon: 'warning',
+                  buttons: {
+                    cancel: true,
+                    confirm: {
+                      text: 'delete',
+                      value: info,
+                    },
+                  },
+                }).then((value) => {
+                  if (value) {
+                    this.props.removeEvent(value.event);
+                    swal({
+                      title: 'Event Deleted',
+                      icon: 'success',
+                    });
+                  }
+                });
+              });
+            }}
+            dateClick={(evt) =>
+              swal({
+                title: 'Create Event',
+                content: <MyInput date={evt} />,
+                buttons: {
+                  cancel: true,
+                  confirm: 'Add Event',
+                },
+              }).then((val) => {
+                console.log('oranges', val.value);
+                this.props.addEvent(this.props.user, val.value);
+                swal({
+                  title: 'Event Created',
+                  text: 'Event: ' + val.value.title + ', was created!',
+                  icon: 'success',
+                });
+              })
+            }
+          />
+        </div>
+      </div>
+    );
+  }
 }
-const mapDispatch=(dispatch)=>({
-  getCalendar: (user)=>dispatch(fetchEvents(user)),
-  addEvent:(user,event)=>dispatch(newEvent(user,event)),
-  updateEvent:(user,event)=>dispatch(changeEvent(user,event)),
-  removeEvent:(event)=>dispatch(deleteEvent(event))
-})
-const mapState=(state)=>({
-  user:state.user,
-  events:state.calendar,
-  meetings:state.meetings
-})
+const mapDispatch = (dispatch) => ({
+  getCalendar: (user) => dispatch(fetchEvents(user)),
+  addEvent: (user, event) => dispatch(newEvent(user, event)),
+  updateEvent: (user, event) => dispatch(changeEvent(user, event)),
+  removeEvent: (event) => dispatch(deleteEvent(event)),
+});
+const mapState = (state) => ({
+  user: state.user,
+  events: state.calendar,
+  meetings: state.meetings,
+});
 
-export default connect(mapState,mapDispatch)(Calendar)
-
+export default connect(mapState, mapDispatch)(Calendar);
 
 // export default Calendar
-
