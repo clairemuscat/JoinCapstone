2c2ff460e0368ee0a7794da3f73ed372f921fb11
Jonah Ullman <jonahmullman@gmail.com>
2020-05-24 19:49:55 -0500

    nav link underline

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
-import { withRouter } from "react-router-dom";
-import firebase from "firebase";
+import React, { useState } from 'react';
+import { withRouter } from 'react-router-dom';
+import firebase from 'firebase';
 
-const Navbar = withRouter(function ({ history }) {
+const Navbar = withRouter(function ({ history, location }) {
   const linkTo = (path) => {
     history.push(path);
   };
@@ -10,24 +10,42 @@ const Navbar = withRouter(function ({ history }) {
   const handleSignOut = () => {
     try {
       firebase.auth().signOut();
-      history.push("/");
+      history.push('/');
     } catch (error) {}
   };
-
+  console.log(location);
   return (
     <div id="navbar">
       <img src="/logo.png" />
       <div id="navbar-link-container">
-        <div className="navbar-link" onClick={() => linkTo("/connect")}>
+        <div
+          className={
+            location.pathname === '/connect'
+              ? 'navbar-link underline'
+              : 'navbar-link'
+          }
+          onClick={() => linkTo('/connect')}
+        >
           Connect
         </div>
         <div
-          className="navbar-link"
-          onClick={() => linkTo("/account/calendar")}
+          className={
+            location.pathname === '/account/calendar'
+              ? 'navbar-link underline'
+              : 'navbar-link'
+          }
+          onClick={() => linkTo('/account/calendar')}
         >
           Calendar
         </div>
-        <div className="navbar-link" onClick={() => linkTo("/connections")}>
+        <div
+          className={
+            location.pathname === '/connections'
+              ? 'navbar-link underline'
+              : 'navbar-link'
+          }
+          onClick={() => linkTo('/connections')}
+        >
           Connections
         </div>
         <div className="navbar-link" onClick={handleSignOut}>
