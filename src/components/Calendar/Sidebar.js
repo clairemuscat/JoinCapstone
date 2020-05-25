9a79673f835f7111f34af8e762db2c4d16430be2
Symone Varnado <symonevarnado@gmail.com>
2020-05-25 09:44:25 -0500

    calendar done...for now

--- src/components/Calendar/Sidebar.js
+++ src/components/Calendar/Sidebar.js
@@ -20,8 +20,10 @@ class Sidebar extends React.Component{
     render(){
         const upcoming = this.props.upcoming
         const meetings = this.props.meetings
-        console.log('lime',meetings)
         const user=this.props.user
+        upcoming.filter(meeting=> meeting.start = new Date(meeting.start).toUTCString())
+        meetings.filter(meeting=> meeting.start = new Date(meeting.start).toUTCString())
+        
         return( <section className="sidebar">
         <div className="sidebar-header">
           <h3>
@@ -31,12 +33,12 @@ class Sidebar extends React.Component{
         <div className='meeting-section'>
         <div className='meeting-list'>
         <h5>UPCOMING</h5>
-    { upcoming.map(meeting => meeting.inviteFirst ? <div>{meeting.title} @ {meeting.start}<br/>attendee(s): {meeting.host},{meeting.inviteFirst} {meeting.inviteLast} <button type='button' id='start-call-button'>Start Meeting</button><button type='button'>Reschedule</button><button type='button'>Cancel</button></div>:<div>{meeting.title} @ {meeting.start}<br/>attendee(s): {meeting.host}</div>)}
+    {upcoming.map(meeting => meeting.inviteFirst ? <div>{meeting.title} @ {meeting.start}<br/>attendee(s): {meeting.host},{meeting.inviteFirst} {meeting.inviteLast} <button type='button' id='start-call-button'>Start Meeting</button><button type='button'>Reschedule</button><button type='button'>Cancel</button></div>:<div>{meeting.title} @ {meeting.start}<br/>attendee(s): {meeting.host}</div>)}
         </div>
         <div>
         <h5 className='meeting-list'>PENDING</h5>
-        {meetings.map(meeting=>{if(meeting.host=== (user.uid)) {return <div className='meeting'>{meeting.title} <DeleteMeeting meeting={meeting} host={meeting.host}/> </div>} 
-        else{return <div className='meeting'>{meeting.title} <DeleteMeeting meeting={meeting}/></div>}})}
+        {meetings.map(meeting=>{if(meeting.host=== (user.uid)){ return <div className='meeting'>{meeting.title} @ {meeting.start} <DeleteMeeting meeting={meeting} host={meeting.host}/> </div>} 
+        else{return <div className='meeting'>{meeting.title} @ {meeting.start} <DeleteMeeting meeting={meeting}/></div>}})}
         </div>
         </div>
       </section>)
